e8bdf0479981dda4b02587a685902df4fbe40c9a
Morwenn <morwenn29@hotmail.fr>
2021-05-02 13:37:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+Craftingoloy is a small RDF/XML ontology about general crafting.
+
+Once upon a pandemic, I was stuck at home and decided to give another look to my garden: while barely maintained, I
+knew that it was a land of potential. Armed with curiosity, patience, and more time home than I would have liked, I
+started to methodically identify the flowers, herbs, shrubs, trees and other plants - despite its relatively small
+size, it is home to a bunch of different ecological niches and the number of species to identify was staggering - at
+some point I had already reached a hundred deifferent plant species, and the list was still growing as the seasons
+passed by.
+
+Halfway through that tedious identification process, I decided to read more about those plants: are they edible? Is it
+safe? Are there other historical uses? The cooking sessions began, I discovered new palettes of tastes and the
+possibilities seemed to go even beyond that. It turned out that those plants, when used to their full potential, were
+formidably diverse in their uses.
+
+Fast-forward to a few weeks from this initial commit: my new-found knowledge was disorganized and split in several
+places that I had to find again whenever I needed to use it again. So the next phase of that project began: building a
+knowledge base that could be searched in useful ways. Despite little knowledge about the area, I started to look at
+graph databases and their powerful query languages. After some experimentations, I came to the logical conclusion that
+my starting point should be to create an ontology - the database and queries would come later.
+
+And that small ontology is the result of that work: a single RDF/XML file created with owlready2. The data used to fill
+the quadstore is not part of this project because it is somewhat personal (and it's also mostly in French because it is
+the language I use to reason about plants, resources and crafting), but the ontology can freely be used for similar
+projects. Hope you enjoyed the pompous story.
+
+Yours truly,
+
+Morwenn
---
+++ create-ontology.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2021 Morwenn
+# SPDX-License-Identifier: BSL-1.0
+
+import sys
+
+import owlready2
+
+
+# Create world and ontology
+owlready2.default_world.set_backend(filename=sys.argv[1])
+crafting = owlready2.get_ontology("http://morwenn.bzh/crafting.owl")
+
+with crafting:
+
+    class link(owlready2.DataProperty, owlready2.FunctionalProperty):
+        """
+        A link can be attached to any entity for additional explanations.
+        """
+        domain = [owlready2.Thing]
+        range = [str]
+
+    ##################################################
+    # Provider
+
+    class Provider(owlready2.Thing):
+        pass
+
+    class supergroups(Provider >> Provider):
+        pass
+
+    ##################################################
+    # Product
+
+    class Product(Provider):
+        pass
+
+    class description(owlready2.DataProperty, owlready2.FunctionalProperty):
+        domain = [Product]
+        range = [str]
+
+    class provides(Provider >> Product):
+        """
+        This one is for products that can be trivially extracted
+        from plants to provide an ingredient, without having to
+        go through a recipe.
+
+        For example an egg can provide egg white and egg yolk by
+        simple separation, without any other ingredient nor tool,
+        so we can avoid having to write a recipe for that.
+        """
+        pass
+
+    class is_provided_by(Product >> Provider):
+        inverse_property = provides
+
+    ##################################################
+    # Plant
+
+    class Plant(Provider):
+        """
+        Plants are a type of provider identified by their Latin
+        name. They generally provide ingredients under their
+        common name.
+        """
+        pass
+
+    class is_somewhat_edible(owlready2.DataProperty, owlready2.FunctionalProperty):
+        """
+        Whether some parts of the plant, whether raw or processed,
+        are edible and can be used in the kitchen.
+        """
+        domain = [Plant]
+        range = [bool]
+
+    ##################################################
+    # Ingredient
+
+    class Ingredient(Product):
+        """
+        Ingredients are products that will be consumed by recipes
+        and are not expected to remain once the recipe is done.
+        """
+        pass
+
+    ##################################################
+    # Tool
+
+    class Tool(Product):
+        """
+        Tools are pretty self-explanatory: a recipe can use tools
+        to make new products, but they are not expect to disappear
+        one the recipe is done. They are also products because it
+        is totally possible towrite a recipe to create new tools.
+        """
+        pass
+
+    ##################################################
+    # Recipe
+
+    class Recipe(owlready2.Thing):
+        """
+        A recipe uses ingredients and tools to produce new products.
+        The ingredients are consumed and should disappear by the time
+        the recipe is done, while tools remain.
+
+        The ontology is designed in such a way that several recipes
+        can use similar ingredients to produce similar products.
+        Supporting such redundancy is a feature.
+        """
+        pass
+
+    class uses(Recipe >> Product):
+        pass
+
+    class is_used_by(Product >> Recipe):
+        inverse_property = uses
+
+    class produces(Recipe >> Product):
+        pass
+
+    class is_produced_by(Product >> Recipe):
+        inverse_property = produces
+
+    class text(owlready2.DataProperty, owlready2.FunctionalProperty):
+        domain = [Recipe]
+        range = [str]
+
+
+crafting.save(file="crafting.owl", format="rdfxml")
+owlready2.default_world.save()
